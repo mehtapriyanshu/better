8203fdfaf3446b36c5e6690313b27501c5817da3
Priyanshu Mehta <priyanshumehta054@gmail.com>
2024-09-18 17:17:31 +0530

    modifiy

--- public/index.php
+++ public/index.php
@@ -1,20 +1,15 @@
 <?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use app\Router;
-use app\controllers\Productcontroller;
-
+require_once __DIR__ . '/../vendor/autoload.php'; 
+use app\Router; 
+use app\controllers\Productcontroller; 
 $router = new Router();
-
+$productcontroller = new Productcontroller(); 
 // Define routes
-$router->get('/', [Productcontroller::class, 'index']);
-$router->get('/products', [Productcontroller::class, 'index']);
-$router->get('/products/create', [Productcontroller::class, 'create']);
-$router->post('/products/create', [Productcontroller::class, 'create']);
-$router->get('/products/update', [Productcontroller::class, 'update']);
-$router->post('/products/update', [Productcontroller::class, 'update']);
-$router->post('/products/delete', [Productcontroller::class, 'delete']);
-
-// Resolve the current URL
+$router->get('/', [$productcontroller, 'index']); 
+$router->get('/products', [$productcontroller, 'index']);
+$router->get('/products/create', [$productcontroller, 'create']);
+$router->post('/products/create', [$productcontroller, 'create']);
+$router->get('/products/update', [$productcontroller, 'update']); 
+$router->post('/products/update', [$productcontroller, 'update']);
+$router->post('/products/delete', [$productcontroller, 'delete']); // Resolve the current URL 
 $router->resolve();
